23381df35d4256bc7ff1d40881811396486990b6
yousef mohamed <joetiger22222@gmail.com>
2023-08-15 14:59:07 +0300

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-personal-website2';
+  download(){
+    var pdfUrl='assets/Youssef Mohammed.pdf';
+    window.open(pdfUrl,'_blank');
+  }
+  scrollToAboutMe() {
+    var aboutElement = document.getElementById('aboutMe');
+    if (aboutElement) {
+      window.scrollTo({
+        top: aboutElement.offsetTop,
+        behavior: 'smooth' // Optional: Adds smooth scrolling animation
+      });
+    }
+  }
+  scrollToExp(){
+    var expElement = document.getElementById('experience');
+    if(expElement){
+      window.scrollTo({
+        top: expElement.offsetTop,
+        behavior: 'smooth' // Optional: Adds smooth scrolling animation
+      });
+    }
+  }
+  scrollToProjects(){
+    var projects = document.getElementById('projects');
+    if(projects){
+      window.scrollTo({
+        top: projects.offsetTop,
+        behavior: 'smooth' // Optional: Adds smooth scrolling animation
+      });
+    }
+  }
+  
 }
